d50e9d139d3d68d3a377a5d47ad3c1aa924e0b69
rob587 <robertocamm02@yahoo.com>
2025-07-09 16:36:45 +0200

    primo step

--- js/script.js
+++ js/script.js
@@ -2,3 +2,7 @@ const accendiBtn = document.getElementById('btn-on')
 const spegniBtn = document.getElementById('btn-off')
 const image = document.getElementById('image')
 
+accendiBtn.addEventListener('click', function(){
+    image.src = 'img/yellow_lamp.png'
+});
+
